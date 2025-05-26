356dc0fdcb879c192ba0fdec9a61fe890ef24091
janjettmann <janjettmann1@gmail.com>
2025-05-26 18:47:33 +0200

    S now gets read from the DAM results file as well instead of being hardcoded

--- codes/preprocessing.py
+++ codes/preprocessing.py
@@ -67,13 +67,14 @@ class PreProcessor:
         """
         # initialize empty structure
         data = {}#{t: {} for t in range(1, 25)}
-
+        scenarios = []
         with open(f'{filename}.txt') as f:
             for lineno, line in enumerate(f, start=1):
                 parts = line.strip().split()
                 if not parts:
                     continue  # skip empty lines
                 s = int(parts[0])  # scenario index
+                scenarios.append(s)
                 values = list(map(float, parts[2:]))  # drop parts[1]=probability
                 if len(values) != 24:
                     raise ValueError(
@@ -83,7 +84,7 @@ class PreProcessor:
                 for t, v in enumerate(values, start=1):
                     data[(t, s)] = v
 
-        return data
+        return data, scenarios
 
     @staticmethod
     def get_sorted_bid_curve(ld,S, t):
@@ -93,35 +94,31 @@ class PreProcessor:
     def get_dam_quantities(self):
 
         """
-        This function loads the DAM results, meaning the bid pairs of energy and price, and using those calculates
+        This function loads the DAM results, meaning the bid pairs of energy and price, and using those, calculates
         the matched energy of the DAM. For each hour, the point at the bid curve
         """
 
-        #ToDo: MAJOR PROBLEM: this assumes that lD is the prices ordered in ascending order which they are  not!
-
         path = "/Users/janjettmann/temp/ftc_10_2023_results"
 
-        e_da_m = self.read_from_txt(path+"/eDA_m")
-        e_da_p = self.read_from_txt(path+"/eDA_p")
-        ld = self.read_from_txt(path+"/lD")
+        e_da_m_from_DAM_run, S = self.read_from_txt(path+"/eDA_m")
+        e_da_p_from_DAM_run, _ = self.read_from_txt(path+"/eDA_p")
+        ld_from_DAM_run, _ = self.read_from_txt(path+"/lD")
         x=1
-        S = [1,2,4,5,6,8,9,10] #ToDo: for now this is hardcoded but it should obv not be
 
         e_da_m_matched = {}
         e_da_p_matched = {}
         for t in range(1, self.scenario_data["nT"] + 1):
             real_price = self.scenario_data["lD"][t,1] # scenario doesn't matter
-            diff =  10**5
             counter = 0 #first element
-            sorted_bid_prices = self.get_sorted_bid_curve(ld,S,t)
+            sorted_bid_prices = self.get_sorted_bid_curve(ld_from_DAM_run,S,t)
 
             while sorted_bid_prices[counter][0] <= real_price:
                 if counter == len(S) - 1:
                     break
                 counter = counter+1
-                closest_scenario_index = sorted_bid_prices[counter][1]
-            e_da_m_matched[t] = e_da_m[t,closest_scenario_index]
-            e_da_p_matched[t] = e_da_p[t,closest_scenario_index]
+                closest_scenario = sorted_bid_prices[counter][1]
+            e_da_m_matched[t] = e_da_m_from_DAM_run[t,closest_scenario]
+            e_da_p_matched[t] = e_da_p_from_DAM_run[t,closest_scenario]
 
         e_da_m = {}
         e_da_p = {}
@@ -132,7 +129,7 @@ class PreProcessor:
         self.scenario_data["eDA_p"] = e_da_p
         self.scenario_data["eDA_m"] = e_da_m
 
-    ToDo: debug this  - check if the interception points are actually the right ones!!
+    #ToDo: debug this  - check if the interception points are actually the right ones!!
 
     def allocate_lr_and_ld(self):
 
